8da9341785ea06666d16238b7af17212c63afd6d
Han Sun <hanicesun>
2026-01-26 23:41:07 -0800

    hs

--- src/hlarchical/trainer.py
+++ src/hlarchical/trainer.py
@@ -17,7 +17,8 @@ class Trainer:
             self.test_dataset = torch.load(test_file, weights_only=False)
 
         self.model_name = model_name
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
+
         print(f'using device: {self.device}', flush=True)
         model_class = eval(self.config['models'][model_name]['class'])
         cfg = Config(self.config['models'][model_name]['params'])
