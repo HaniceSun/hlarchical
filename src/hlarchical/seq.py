4ff53af3d5a280d9b9a76255b636634e9ec8af0f
Hanice Sun <sunhanice@gmail.com>
2026-03-04 14:17:59 -0800

    hanice

--- src/hlarchical/seq.py
+++ src/hlarchical/seq.py
@@ -4,7 +4,7 @@ class Seq:
     def __init__(self):
         pass
 
-    def subset_cram_to_fastq(self, cram_file=['HPAP001.cram'], fasta_file='Homo_sapiens_assembly38.fasta', bed_file='keep_chroms.bed', out_file='subset_cram_to_fastq.sh', n_thresholds=2):
+    def subset_cram_to_fastq(self, cram_file=['HPAP001.cram'], fasta_file='Homo_sapiens_assembly38.fasta', bed_file='keep_chroms.bed', out_file='subset_cram_to_fastq.sh', n_thresholds=4):
         '''
         bed_file should include chr6 plus other chroms other than chr1-chr5, chr7-chr22, chrX, chrY, chrMT, such as chr1_KI270706v1_random and chrUn_KI270302v1 etc.
         '''
@@ -18,7 +18,7 @@ class Seq:
                 outfile.write(cmd + '\n')
                 outfile.write(cmd2 + '\n')
 
-    def run_hlahd(self, fq1_file=['HPAP001_subset.R1.fastq.gz'], freq_data='freq_data', dict_file='dictionary', gene_split_file='HLA_gene.split.txt', out_file='run_hlahd.sh', out_dir='HLA-HD', n_thresholds=2, min_read_length=100, trim_rate=0.95):
+    def run_hlahd(self, fq1_file=['HPAP001_subset.R1.fastq.gz'], freq_data='freq_data', dict_file='dictionary', gene_split_file='HLA_gene.split.txt', out_file='run_hlahd.sh', out_dir='HLA-HD', n_thresholds=4, min_read_length=100, trim_rate=0.95):
         if not os.path.exists(out_dir):
             os.makedirs(out_dir)
 
@@ -29,7 +29,7 @@ class Seq:
                 cmd = f'hlahd.sh -t {n_thresholds} -m {min_read_length} -c {trim_rate} -f {freq_data} {fq1} {fq2} {gene_split_file} {dict_file} {sample_id} {out_dir}'
                 outfile.write(cmd + '\n')
 
-    def run_xhla(self, fq1_file=['HPAP001_subset.R1.fastq.gz'], sif_file='xhla.sif', fasta_file='Homo_sapiens_assembly38.fasta', out_file='run_xhla.sh', n_thresholds=2, out_dir='xHLA'):
+    def run_xhla(self, fq1_file=['HPAP001_subset.R1.fastq.gz'], sif_file='xhla.sif', genome_index='GRCh38', out_file='run_xhla.sh', n_thresholds=4, out_dir='xHLA'):
         if not os.path.exists(out_dir):
             os.makedirs(out_dir)
 
@@ -38,7 +38,7 @@ class Seq:
                 fq2 = fq1.replace('.R1.fastq.gz', '.R2.fastq.gz')
                 sample_id = os.path.basename(fq1).split('_subset')[0]
                 bam_file = f'{out_dir}/{sample_id}.bam'
-                cmd = f'bwa mem -t {n_thresholds} {fasta_file} {fq1} {fq2} | samtools sort -@ {n_thresholds} -o {bam_file}; samtools index {bam_file}'
+                cmd = f'bwa mem -t {n_thresholds} {genome_index} {fq1} {fq2} | samtools sort -@ {n_thresholds} -o {bam_file}; samtools index {bam_file}'
                 outfile.write(cmd + '\n')
                 cmd = f'singularity exec -B `pwd`:`pwd` --pwd `pwd` {sif_file} run.py --sample_id {sample_id} --input_bam_path {bam_file} --output_path {out_dir}/{out_dir}'
                 outfile.write(cmd + '\n')
