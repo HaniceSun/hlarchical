0e23597659ceaa8235ef5889ec0b2d965d87ba5a
Hanice Sun <sunhanice@gmail.com>
2026-04-14 22:13:41 -0700

    hanice

--- src/hlarchical/preprocess.py
+++ src/hlarchical/preprocess.py
@@ -9,9 +9,9 @@ class Preprocessor:
         self.hla_chrom = ['6', 'chr6']
         self.ped_cols = ['FID', 'IID', 'PID', 'MID', 'SEX', 'PHENOTYPE'] + [f'{x}_{a}' for x in self.HLA for a in ['A1', 'A2']]
 
-    def bed_to_vcf(self, in_file='HAPMAP_CEU.bed', input_genome_build='hg18', output_genome_build='GRCh37'):
+    def bed_to_vcf(self, in_file='GDA.bed', input_genome_build='GRCh37', output_genome_build='GRCh37'):
         bfile = in_file.split('.bed')[0]
-        cmd = f'plink2 --bfile {bfile} --recode vcf bgz --out {bfile}'
+        cmd = f'plink2 --bfile {bfile} --vcf vcf bgz --out {bfile}'
         print(cmd)
         subprocess.run(cmd, shell=True)
 
@@ -22,11 +22,9 @@ class Preprocessor:
         cmd = f'bcftools view -S {samples_sorted} {bfile}.vcf.gz -Oz -o {bfile}_sampleSorted.vcf.gz; rm {bfile}.vcf.gz'
         print(cmd)
         subprocess.run(cmd, shell=True)
+
         fasta_source = self.get_genome_reference(genome_build=input_genome_build)
         fasta_dest = self.get_genome_reference(genome_build=output_genome_build)
-        cmd = f'bcftools norm -m-any --check-ref s -f {fasta_source} {bfile}_sampleSorted.vcf.gz -Oz -o {bfile}_norm.vcf.gz'
-        print(cmd)
-        subprocess.run(cmd, shell=True)
         if input_genome_build != output_genome_build:
             self.vcf_liftover(f'{bfile}_sampleSorted.vcf.gz', out_file=f'{bfile}_lifted.vcf.gz', input_genome_build=input_genome_build, output_genome_build=output_genome_build)
             cmd = f'bcftools sort {bfile}_lifted.vcf.gz -Oz -o {bfile}_posSorted.vcf.gz; bcftools index {bfile}_posSorted.vcf.gz'
@@ -37,12 +35,8 @@ class Preprocessor:
             cmd = f'bcftools sort {bfile}_sampleSorted.vcf.gz -Oz -o {bfile}_posSorted.vcf.gz; bcftools index {bfile}_posSorted.vcf.gz'
             print(cmd)
             subprocess.run(cmd, shell=True)
-        cmd = f'bcftools norm -m-any --check-ref s -f {fasta_dest} {bfile}_posSorted.vcf.gz -Oz -o {bfile}.vcf.gz'
-        print(cmd)
-        subprocess.run(cmd, shell=True)
         os.remove(f'{bfile}_sampleSorted.vcf.gz')
         os.remove(f'{bfile}_posSorted.vcf.gz')
-        os.remove(f'{bfile}_norm.vcf.gz')
 
     def vcf_liftover(self, in_file, out_file, input_genome_build='hg18', output_genome_build='GRCh37'):
         if input_genome_build in ['hg18', 'NCBI36'] and output_genome_build in ['GRCh37', 'hg19']:
@@ -121,32 +115,25 @@ class Preprocessor:
             pos = df_pos.iloc[n, 2]
             pos_dict[gene] = (chrom, pos)
 
-        df = pd.read_table(in_file, header=None, sep='\t', dtype=str)
-        if df.shape[1] == len(self.ped_cols):
-            df.columns = self.ped_cols
-        else:
-            raise ValueError('The number of columns in the PED file does not match the expected number of columns.')
+        df = pd.read_table(in_file, header=0, sep='\t', dtype=str)
 
         D = {}
         A = []
         S = []
-        for n in range(df.shape[0]):
-            row = df.iloc[n]
-            fid = row['FID']
-            iid = row['IID']
-            k = '_'.join([fid, iid])
+        for gi, g in df.groupby('SampleID'):
+            k = gi
             if k not in S:
                 S.append(k)
             D.setdefault(k, {})
             for gene in self.HLA:
-                for a in ['A1', 'A2']:
+                for a in ['Allele1', 'Allele2']:
                     allele2d = '.'
                     allele4d = '.'
-                    allele = row[f'{gene}_{a}']
-                    if alele not in ['.', './.', '0', 'NA']:
+                    allele = g
+                    if allele not in ['.', './.', '0', 'NA']:
                         alleles = allele.split(':')
                         allele2d = f'{gene}:{alleles[0]}'
-                        allele4d = f'{gene}:{alleles[0]}:{allele[1]}'
+                        allele4d = f'{gene}:{alleles[0]}:{alleles[1]}'
                         D[k].setdefault(a, [])
                         D[k][a].append(allele2d)
                         D[k][a].append(allele4d)
@@ -193,14 +180,133 @@ class Preprocessor:
         print(cmd)
         subprocess.run(cmd, shell=True)
 
-    def make_reference(self, in_file=['HAPMAP_CEU.vcf.gz', 'HAPMAP_CEU_HLA.vcf.gz'], maker_file=None, out_file='HAPMAP_CEU_REF_phased.vcf.gz', burnin=10, iterations=15):
+    def hlarchical_table_to_vcf(self, in_file='1000G_WGS_HLA-HD.txt', genome_build='GRCh37', hla_pos_file='HLA_gene_position.txt'):
+        hla_pos_file = hla_pos_file.split('.txt')[0] + f'_{genome_build}.txt'
+        if os.path.exists(hla_pos_file) == False:
+            self.get_hla_position(out_file=hla_pos_file, genome_build=genome_build)
+        df_pos = pd.read_table(hla_pos_file, header=None, sep='\t', dtype=str)
+        pos_dict = {}
+        for n in range(df_pos.shape[0]):
+            gene = df_pos.iloc[n, 0]
+            chrom = df_pos.iloc[n, 1]
+            pos = df_pos.iloc[n, 2]
+            pos_dict[gene] = (chrom, pos)
+
+        df = pd.read_table(in_file, header=0, sep='\t', dtype=str)
+
+        D = {}
+        A = []
+        S = []
+        for k, g in df.groupby('SampleID'):
+            if k not in S:
+                S.append(k)
+            D.setdefault(k, {})
+            for gene in self.HLA:
+                for a in ['Allele1', 'Allele2']:
+                    allele2d = '.'
+                    allele4d = '.'
+                    allele = g.loc[g['HLA'] == gene, a].values[0]
+                    D[k].setdefault(a, [])
+                    if allele not in ['.', './.', '0', 'NA']:
+                        alleles = allele.split(':')
+                        allele2d = f'{alleles[0]}:{alleles[1]}'
+                        allele4d = f'{alleles[0]}:{alleles[1]}:{alleles[2]}'
+                        D[k][a].append(allele2d)
+                        D[k][a].append(allele4d)
+                        A.append(allele2d)
+                        A.append(allele4d)
+
+        L = []
+        for allele in sorted(set(A)):
+            gene = allele.split(':')[0]
+            if gene in pos_dict:
+                chrom, pos = pos_dict[gene]
+                ref = 'A'
+                alt = 'P'
+                row = [chrom, pos, allele, ref, alt, '.', 'PASS', '.', 'GT']
+                for sample in S:
+                    if allele in D[sample]['Allele1'] and allele in D[sample]['Allele2']:
+                        gt = '1/1'
+                    elif allele in D[sample]['Allele1'] or allele in D[sample]['Allele2']:
+                        gt = '0/1'
+                    else:
+                        gt = '0/0'
+                    row.append(gt)
+                L.append(row)
+
+        bfile = in_file.split('.txt')[0]
+        df = pd.DataFrame(L)
+        df.columns = ['#CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUAL', 'FILTER', 'INFO', 'FORMAT'] + S
+        out_file = f'{bfile}.vcf'
+        with open(out_file, 'w') as outfile:
+            outfile.write('##fileformat=VCFv4.2\n')
+            outfile.write('##source=VCFPhaser\n')
+            outfile.write('##contig=<ID=6>\n')
+            outfile.write('##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">\n')
+        df.to_csv(out_file, sep='\t', index=False, mode='a')
+
+        samples_sorted = f'{bfile}_samples_sorted.txt'
+        cmd = f'bcftools query -l {out_file} | sort > {samples_sorted}'
+        print(cmd)
+        subprocess.run(cmd, shell=True)
+        cmd = f'bcftools view -S {samples_sorted} {out_file} -Oz -o {bfile}_sampleSorted.vcf.gz; rm {out_file}'
+        print(cmd)
+        subprocess.run(cmd, shell=True)
+        cmd = f'bcftools sort {bfile}_sampleSorted.vcf.gz -Oz -o {bfile}.vcf.gz; rm {bfile}_sampleSorted.vcf.gz; bcftools index {bfile}.vcf.gz'
+        print(cmd)
+        subprocess.run(cmd, shell=True)
+
+    def subset_samples_vcf(self, vcf_file, sample_list, out_file, n_threads=4):
+        cmd = f'bcftools view --threads {n_threads} -S {sample_list} {vcf_file} -Oz -o {out_file}; tabix -p vcf {out_file}'
+        print(cmd)
+        subprocess.run(cmd, shell=True)
+
+    def subset_variants_vcf(self, vcf_file, genome_build='GRCh37', flank=1e6, n_threads=4):
+        hla_pos_file=f'HLA_gene_position_{genome_build}.txt' 
+        if os.path.exists(hla_pos_file) == False:
+            self.get_hla_position(out_file=hla_pos_file, genome_build=genome_build)
+        out_file = vcf_file.split('.vcf.gz')[0] + '_variantSubset.vcf.gz'
+        df = pd.read_table(hla_pos_file, header=None, sep='\t')
+        chrom = df.iloc[0, 1].astype(str)
+        start = df.iloc[:, 2].min() - int(flank)
+        end = df.iloc[:, 2].max() + int(flank)
+        cmd = f'bcftools view --threads {n_threads} -r {chrom}:{start}-{end} {vcf_file} -Oz -o {out_file}; tabix -p vcf {out_file}'
+        print(cmd)
+        subprocess.run(cmd, shell=True)
+
+    def fixref_vcf(self, vcf_file, genome_build='GRCh37'):
+        out_file = vcf_file.split('.vcf')[0] + '_fixref.vcf.gz'
+        fasta_file = self.get_genome_reference(genome_build=genome_build)
+        # flip/swap using fixref
+        cmd = f'bcftools +fixref {vcf_file} -Oz -o {out_file} -- -d -f {fasta_file} -m flip'
+        print(cmd)
+        subprocess.run(cmd, shell=True)
+        # stats after fixref
+        cmd = f'bcftools +fixref {out_file} -- -f {fasta_file}'
+        print(cmd)
+        subprocess.run(cmd, shell=True)
+
+    def make_reference(self, ref_variant_vcf='1000G_array_broad_overlappedWGS_variantSubset_fixref.vcf.gz', ref_hla_vcf='1000G_WGS_HLA-HD_overlappedBroad.vcf.gz', out_file='1000G_REF_phased.vcf.gz', marker_file=None, burnin=10, iterations=15):
+        cmd = f'bcftools query -l {ref_variant_vcf}'
+        result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
+        ref_variant_samples = result.stdout.strip().split('\n')
+        print(f'Number of samples in variant VCF: {len(ref_variant_samples)}')
+        cmd = f'bcftools query -l {ref_hla_vcf}'
+        result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
+        ref_hla_samples = result.stdout.strip().split('\n')
+        print(f'Number of samples in HLA VCF: {len(ref_hla_samples)}')
+        if ref_variant_samples != ref_hla_samples:
+            print('Error: Sample names in variant and HLA VCF files do not match.')
+            return
+
         concated_file = out_file.split('_phased')[0] + '_concated.vcf.gz'
-        cmd = f'bcftools concat {" ".join(in_file)} -Oz -o {concated_file}'
+        bfile = concated_file.split('.vcf.gz')[0]
+        pos_uniq_file = bfile + '_posUniq.vcf.gz'
+        cmd = f'bcftools concat {" ".join([ref_variant_vcf, ref_hla_vcf])} -Oz -o {concated_file}'
         print(cmd)
         subprocess.run(cmd, shell=True)
         self.vcf_pos_unique(concated_file)	
-        bfile = concated_file.split('.vcf.gz')[0]
-        cmd = f'bcftools sort {bfile}_PosUniq.vcf.gz -Oz -o {concated_file}; rm {bfile}_PosUniq.vcf.gz; bcftools index {concated_file}'
+        cmd = f'bcftools sort {pos_uniq_file} -Oz -o {concated_file}; bcftools index {concated_file}; rm {pos_uniq_file}'
         print(cmd)
         subprocess.run(cmd, shell=True)
         cmd = f'beagle gt={concated_file} out={out_file.split(".vcf")[0]} burnin={burnin} iterations={iterations}'
@@ -209,21 +315,19 @@ class Preprocessor:
         os.remove(concated_file)
         os.remove(f'{concated_file}.csi')
 
-    def phase_sample(self, sample_file='GDA.vcf.gz', ref_file='HAPMAP_CEU_REF_phased.vcf.gz', out_file='GDA_phased_HAPMAP_CEU_REF.vcf.gz', sample_build='GRCh37', ref_build='GRCh37'):
-        bfile = sample_file.split('.vcf')[0].split('.bed')[0]
-        if sample_file.find('.vcf') == -1:
-            cmd = f'plink2 --bfile {bfile} --chr 6 --recode vcf bgz --out {bfile}'
-            print(cmd)
-            subprocess.run(cmd, shell=True)
-        if sample_build == ref_build:
-            fasta_file = self.get_genome_reference(genome_build=sample_build)
-            cmd = f'bcftools norm -m-any --check-ref s -f {fasta_file} {bfile}.vcf.gz -Oz -o {bfile}_norm.vcf.gz'
-            print(cmd)
-            subprocess.run(cmd, shell=True)
-            cmd = f'beagle gt={bfile}_norm.vcf.gz ref={ref_file} out={out_file.split(".vcf")[0]}'
-            print(cmd)
-            subprocess.run(cmd, shell=True)
-            #os.remove(f'{bfile}_norm.vcf.gz')
+    def phase_sample_on_reference(self, sample_vcf='GDA.vcf.gz', ref_vcf='1000G_REF_phased.vcf.gz', genome_build='GRCh37', subset_variants=True, fix_ref=True, flank=1e6):
+        out_file = sample_vcf.split('.vcf')[0] + '_phased_on_' + ref_vcf
+        if subset_variants:
+            print('subsetting sample variants to HLA region...')
+            self.subset_variants_vcf(sample_vcf, genome_build=genome_build, flank=flank)
+            sample_vcf = sample_vcf.split('.vcf')[0] + '_variantSubset.vcf.gz'
+        if fix_ref:
+            print('refix on sample VCF...')
+            self.fixref_vcf(sample_vcf, genome_build=genome_build)
+            sample_vcf = sample_vcf.split('.vcf')[0] + '_fixref.vcf.gz'
+        cmd = f'beagle gt={sample_vcf} ref={ref_vcf} out={out_file.split(".vcf")[0]}'
+        print(cmd)
+        subprocess.run(cmd, shell=True)
 
     def get_genome_reference(self, genome_build='GRCh37'):
         if genome_build in ['GRCh38', 'hg38']:
@@ -297,7 +401,7 @@ class Preprocessor:
 
     def vcf_pos_unique(self, in_file):
         seen = {}
-        out_file = in_file.replace('.vcf', '_PosUniq.vcf')
+        out_file = in_file.replace('.vcf', '_posUniq.vcf')
         if in_file.endswith('.vcf.gz'):
             infile = gzip.open(in_file, 'rt')
             outfile = gzip.open(out_file, 'wt')
@@ -325,6 +429,5 @@ class Preprocessor:
 if __name__ == '__main__':
     pp = Preprocessor()
     pp.bed_to_vcf()
-    pp.ped_to_vcf()
     pp.make_reference()
-    pp.phase_sample(sample_file='GDA.bed')
+    pp.phase_sample()
