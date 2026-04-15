f1118372f76234ee23f8f2a04e449017df65c6f3
Hanice Sun <sunhanice@gmail.com>
2026-04-14 21:28:54 -0700

    hanice

--- src/hlarchical/process.py
+++ src/hlarchical/process.py
@@ -3,7 +3,7 @@ import pandas as pd
 import gzip
 
 class Processor:
-    def __init__(self, ref_phased='EuropeanREF_phased.vcf.gz', label_include=['HLA'], feature_exclude=['HLA', 'AA_'], hla_renaming=True, expert_by='ld'):
+    def __init__(self, ref_phased='1000G_REF_phased.vcf.gz', label_include=['HLA'], feature_exclude=['HLA'], expert_by='ld'):
         self.label_include = label_include
         self.feature_exclude = feature_exclude
         self.expert_by = expert_by
@@ -21,11 +21,6 @@ class Processor:
                     break
             wh.append(flag)
         self.ref_phased_target = self.ref_phased[wh].copy()
-
-        self.hla_renaming = hla_renaming
-        if hla_renaming:
-            self.ref_phased_target = self.renaming_hla(self.ref_phased_target)
-        self.ref_phased_target.to_csv('t1.txt', sep='\t', index=False, header=True)
 
         # Extract non-HLA for features
         wh = []
@@ -54,42 +49,9 @@ class Processor:
         self.expert_groups['E2'] = ['HLA-DPA1', 'HLA-DPB1']
         self.expert_groups['E3'] = ['HLA-DQA1', 'HLA-DQB1', 'HLA-DRB1']
 
-    def make_features(self, subset=[], out_file='features.txt'):
-        if subset:
-            wh1 = self.ref_phased_feature['CHROM'] == subset[0]
-            wh2 = self.ref_phased_feature['POS'] >= subset[1]
-            wh3 = self.ref_phased_feature['POS'] <= subset[2]
-            wh = wh1 & wh2 & wh3
-            df = self.ref_phased_feature[wh].copy()
-
+    def make_features(self, out_file='features.txt'):
         df = self.ref_phased_feature.iloc[:, 9:].T
         df.columns = self.ref_phased_feature['ID'] + '_' + self.ref_phased_feature['POS'].astype(str)
-        df.index.name = 'sample'
-        df.to_csv(out_file, sep='\t', index=True, header=True)
-
-    def make_labels(self, out_file='labels.txt', maps_file='maps.txt'):
-        self.maps = pd.read_table(maps_file, header=0, sep='\t', low_memory=False)
-        heads = self.maps['head'].unique().tolist()
-        n_heads = len(heads)
-        samples = self.ref_phased.columns[9:]
-        n_samples = len(samples)
-        D = dict(zip(self.maps['allele'], self.maps['label']))
-        df = pd.DataFrame(index=samples, columns=heads)
-        for n in range(self.ref_phased_target.shape[0]):
-            ID = self.ref_phased_target['ID'].iloc[n]
-            fields = ID.split(':')
-            head = ':'.join(fields[0:-1])
-            head_idx = heads.index(head)
-
-            for m in range(n_samples):
-                A = [0, 0]
-                allele = self.ref_phased.iloc[n, 9 + m].split('|')
-                if len(allele) == 2:
-                    if allele[0] == '1':
-                        A[0] = D[ID]
-                    if allele[1] == '1':
-                        A[1] = D[ID]
-                df.iloc[m, head_idx] = '|'.join([str(x) for x in A])
         df.index.name = 'sample'
         df.to_csv(out_file, sep='\t', index=True, header=True)
 
@@ -160,11 +122,37 @@ class Processor:
                 E.append(expert_id)
             maps['expert'] = E 
 
-        self.maps = maps
-        self.maps.to_csv(out_file, sep='\t', index=False, header=True)
+        maps.to_csv(out_file, sep='\t', index=False, header=True)
         print('processed label maps:')
-        print(self.maps)
+        print(maps)
         print(f'maps data saved to {out_file}')
+
+    def make_labels(self, out_file='labels.txt', maps_file='maps.txt'):
+        maps = pd.read_table(maps_file, header=0, sep='\t', low_memory=False)
+        heads = maps['head'].unique().tolist()
+        n_heads = len(heads)
+        samples = self.ref_phased_target.columns[9:]
+        D = dict(zip(maps['allele'], maps['label']))
+        df = pd.DataFrame('0', index=samples, columns=heads)
+        df1 = pd.DataFrame('0', index=samples, columns=heads)
+        df2 = pd.DataFrame('0', index=samples, columns=heads)
+        for n in range(self.ref_phased_target.shape[0]):
+            ID = self.ref_phased_target['ID'].iloc[n]
+            fields = ID.split(':')
+            head = ':'.join(fields[0:-1])
+            head_idx = heads.index(head)
+            for m in range(len(samples)):
+                allele = self.ref_phased_target.iloc[n, 9 + m].split('|')
+                if len(allele) == 2:
+                    if allele[0] == '1':
+                        df1.iloc[m, head_idx] = D[ID]
+                    if allele[1] == '1':
+                        df2.iloc[m, head_idx] = D[ID]
+        for n in range(df.shape[0]):
+            for m in range(df.shape[1]):
+                df.iloc[n, m] = str(df1.iloc[n, m]) + '|' + str(df2.iloc[n, m])
+        df.index.name = 'sample'
+        df.to_csv(out_file, sep='\t', index=True, header=True)
 
     def make_masks(self, out_file='masks.txt', features_file='features.txt', flank=500000):
         print(self.ref_phased_target)
@@ -221,61 +209,25 @@ class Processor:
         df.to_csv(out_file, sep='\t', index=False, header=True)
         print(f'processed masks data: {out_file}')
 
-    def renaming_hla(self, df):
-        new_ids = []
-        for n in range(df.shape[0]):
-            ID = df['ID'].iloc[n].replace(':', '')
-            fields = ID.split('_')
-            gene = '-'.join(fields[0:2])
-            allele = fields[-1]
-            if len(allele) < 4:
-                new_id = f"{gene}:{allele}"
-            elif len(allele) == 4:
-                new_id = f"{gene}:{allele[0:-2]}:{allele[-2:]}"
-            elif len(allele) == 5:
-                new_id = f"{gene}:{allele[0:-3]}:{allele[-3:]}"
-            elif len(allele) == 6:
-                new_id = f"{gene}:{allele[0:-4]}:{allele[-4:-2]}:{allele[-2:]}"
-            else:
-                new_id = '.'
-                print(f"Warning: unexpected allele: {allele} excluded")
-            new_ids.append(new_id)
-        df['ID'] = new_ids
-        df = df[df['ID'] != '.'].copy()
-        return df
-
-    def prepare_to_predict(self, features_file='features.txt', out_file='to_predict.txt'):
+    def make_features_on_samples(self, sample_phased='', features_file='features.txt', out_file='to_predict.txt'):
+        df = self.read_vcf(sample_phased)
         df_features = pd.read_table(features_file, header=0, sep='\t', low_memory=False)
-        features = []
-        for n in range(1, df_features.shape[1], 2):
-            fields = df_features.columns[n].split('_')
-            k = fields[1]
-            if k not in features:
-                features.append(k)
-
-        df = pd.merge(self.ref_bim, self.sample_phased, left_on='id_ref', right_on='id_sample')
-        wh = df['id_ref'].isin(features)
-        df = df[wh]
-        if df.shape[0] != len(features):
-            print('Warning: some features are missing in the sample phased data')
-
-        M = np.zeros((len(self.sample_ids), len(features) * 2), dtype=int)
-        H = []
-        idx_start = 8
-        for n in range(idx_start, df.shape[1], 2):
-            for j in range(2):
-                wh = (df.iloc[:, n + j] == df['A1_ref']).values.astype(int)
-                for m in range(len(wh)):
-                    M[(n - idx_start) // 2, m * 2 + j] = wh[m]
-                    if j == 0 and n == idx_start:
-                        id_ref = df['id_ref'].iloc[m]
-                        H.append(f'A1_{id_ref}')
-                        H.append(f'A2_{id_ref}')
-
-        df = pd.DataFrame(M)
-        df.index = self.sample_ids
+        samples = df.columns[9:].tolist()
+        features = df_features.columns[1:].tolist()
+        D = {}
+        for n in range(df.shape[0]):
+            k = str(df['ID'].iloc[n]) + '_' + str(df['POS'].iloc[n])
+            D[k] = [x.split(':')[0] for x in df.iloc[n, 9:]]
+        L = []
+        for f in features:
+            if f in D:
+                L.append(D[f])
+            else:
+                L.append(['0|0'] * len(samples))
+        df = pd.DataFrame(L).T
+        df.index = samples
         df.index.name = 'sample'
-        df.columns = H
+        df.columns = features
         df.to_csv(out_file, sep='\t', index=True, header=True)
         print(f'features data to predict saved to {out_file}')
 
@@ -295,10 +247,8 @@ class Processor:
         df.rename(columns={'#CHROM': 'CHROM'}, inplace=True)
         return df
 
-
-
 if __name__ == '__main__':
-    pc = Processor(ref_phased='')
+    pc = Processor()
     pc.make_features()
     pc.make_maps()
     pc.make_labels()
